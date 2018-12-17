038b643ece8daadbb4e768609a84f511ca051858
jabolina <jose.bolina@neppoadm.com.br>
2018-12-17 10:26:14 -0200

    add media decryption function

---
+++ whalesong/src/crypto.js
@@ -0,0 +1,50 @@
+import { decode } from "base-64";
+import { crypto } from "libsignal";
+
+import { CRYPT_KEYS, EMPTY_SALT } from "./constants";
+import { DecryptionError } from "./error";
+
+function hdkf(key, type) {
+    try {
+        return Buffer.concat(crypto.deriveSecrets(
+            Buffer.from(key, "base64"),
+            EMPTY_SALT,
+            Buffer.from(CRYPT_KEYS[type], "hex"),
+            3,
+        ));
+    } catch (error) {
+        throw new DecryptionError(`Error while generating composed token: ${error}`);
+    }
+}
+
+export const isEncrypted = model => model.mediaKey
+    && model.clientUrl
+    && model.clientUrl.endsWith(".enc");
+
+export const decrypt = async (dataBuffer, model) => {
+    let { mediaKey } = model;
+    let composedToken = "";
+
+    try {
+        decode(mediaKey);
+    } catch (error) {
+        try {
+            mediaKey += ("=" * (mediaKey.length % 3));
+            decode(mediaKey);
+        } catch (e) {
+            throw new DecryptionError(`Error in media key: ${e}`);
+        }
+    }
+
+    composedToken = hdkf(mediaKey, model.type);
+
+    const iv = composedToken.slice(0, 16);
+    const cipherKey = composedToken.slice(16, 48);
+    const data = dataBuffer.slice(0, dataBuffer.length - 10);
+
+    try {
+        return crypto.decrypt(cipherKey, data, iv);
+    } catch (error) {
+        throw error;
+    }
+};
